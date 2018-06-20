90f27cb20d8b8aa0055fb32766be1a7dfb0a3e99
Donovan <dpaton.dp@gmail.com>
2018-06-20 12:10:14 -0400

    Sixth Grade Questions

--- data/quiz_questions_json.js
+++ data/quiz_questions_json.js
@@ -4,6 +4,80 @@ const questions=
   // 6th Grade Root Index
   [
 
+ 
+        
+  { "question":"In 1350, in which place is the 0?",
+     "options": ["tens","thousands","ones","hundreds"],
+      "answers":"ones"
+  },
+    
+  {  "question":"In 43,825, what digit is in the thousands place?",
+      "options": [5,4,8,3],
+      "answers":3
+  },
+    
+  {  "question":"How would you write 28 as a Roman numeral?",
+      "options": [XXVIII,MVIII,MCIII,XXIII],
+      "answers":XXVIII
+  },
+
+  {  "question":"A foundation donates $2,000 per year to the state university,How much will the foundation donate in total to the university in 4 years??",
+      "options": [80000,5000,8000,6000],
+      "answers":8000
+  },
+
+  {  "question":"A souvenir factory puts 400 souvenir key chains in each crate. How many key chains will the factory put in 20 crates??",
+      "options": [8000,400000,22000,61000],
+      "answers":8000
+  },
+
+  {  "question":"156 ÷ 39 = __",
+      "options": [16,4,32,17],
+      "answers":4
+  },
+
+  {  "question":"How long was the Oregon Trail?",
+      "options": ["600 miles","1200 miles","2000 miles","3000 miles"],
+      "answers":"2,000 miles"
+  },
+            
+  {  "question":"Physical maps show the ______________?",
+      "options": ["show landforms like deserts, mountains and plains",
+      "climate of an area","natural resources of an area",
+      "size and shape of an area"],
+      "answers":"show landforms like deserts, mountains and plains"
+  },
+            
+  {  "question":"The ______________ are the largest mountain system in North America?",
+      "options": ["Rocky Mountains",
+      "Appalachian Mountains","Nova Scotia Mountains",
+      "Lawrence Mountains"],
+       "answers":"Rocky Mountains"
+  },
+
+  {  "question":" A ___________ creates maps for specific purposes and represents the context in which they were created?",
+      "options": ["economist",
+      "cartographer","biologist",
+      "cardiologist "],
+      "answers":"cartographer"
+  },
+
+  {  "question":" The longest river in World is: ? ",
+      "options": ["Yangtze River",
+      "Amazon River","Mississippi River",
+      "Nile River "],
+      "answers":"Nile River"
+  },
+
+  { "question":" _______________ was an escaped slave who became a writer, orator, journalist , and leading abolitionist.",
+    "options": ["William Lloyd Garrison",
+     "Frederick Douglass","Harriet Tubman",
+      "Dred Scott "],
+      "answers":"Frederick Douglass"
+  }
+          
+          
+             
   ],
   // 7th Grade Root Index
   [ 
