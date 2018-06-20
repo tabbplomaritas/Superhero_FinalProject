5f9fc25b093c06203115c28a4410dcd5e55e50e4
tabbatharenea@gmail.com <tabbatharenea@gmail.com>
2018-06-19 21:13:22 -0400

    updated userform styles and created media queries stylesheet.

--- public/js/components/startgame_comp.js
+++ public/js/components/startgame_comp.js
@@ -4,13 +4,13 @@ console.log("start game comp is alive");
 const startGame ={
 
     template:`
-     <form ng-submit="$ctrl.sendUserInfo($ctrl.user)">
-         <p> Welcome Enter Your Name and Age to defeat Villains of the Galaxy </p>
+     <form class="userInfoForm" ng-submit="$ctrl.sendUserInfo($ctrl.user)">
+         <h3>Enter Your Name and Age to defeat Villains of the Galaxy </h3>
         <input type="text" placeholder="Name" ng-model="$ctrl.user.name">
          <p ng-click="$ctrl.setAge('8-10', $event);">8-10</p>
          <p ng-click="$ctrl.setAge('11-13', $event);">11-13</p>
          <p ng-click="$ctrl.setAge('14-16', $event);">14-16</p>
-        <button> Submit </button>
+        <button> Begin the Adventure </button>
       </form>
     `,
 
