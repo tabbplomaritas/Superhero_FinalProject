979068eb243ae26d56a7edf7ee2de4b9508b2ee4
tabbatharenea@gmail.com <tabbatharenea@gmail.com>
2018-06-20 11:15:32 -0400

    updated styles to be mobile-first and started building out tablet and desktop styles

--- public/js/components/startgame_comp.js
+++ public/js/components/startgame_comp.js
@@ -5,11 +5,14 @@ const startGame ={
 
     template:`
      <form class="userInfoForm" ng-submit="$ctrl.sendUserInfo($ctrl.user)">
+    
          <h3>Enter Your Name and Age to defeat Villains of the Galaxy </h3>
         <input type="text" placeholder="Name" ng-model="$ctrl.user.name">
-         <p ng-click="$ctrl.setAge('8-10', $event);">8-10</p>
-         <p ng-click="$ctrl.setAge('11-13', $event);">11-13</p>
-         <p ng-click="$ctrl.setAge('14-16', $event);">14-16</p>
+        <div class="userInfoForm_Grade">
+            <p ng-click="$ctrl.setAge('8-10', $event);">8-10</p>
+            <p ng-click="$ctrl.setAge('11-13', $event);">11-13</p>
+            <p ng-click="$ctrl.setAge('14-16', $event);">14-16</p>
+         </div>
         <button> Begin the Adventure </button>
       </form>
     `,
@@ -24,7 +27,7 @@ const startGame ={
             console.log($event.target);
             angular.element($event.target).css("background-color", "#FFFFFF");
             vm.user.age = age;
-            console.log(vm.user);
+            console.log(vm.user.age);
         }
 
          vm.sendUserInfo = (user) => {
