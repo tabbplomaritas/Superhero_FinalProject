233899ffdaa8cb4edd817e9f3024713440609df6
tabbatharenea@gmail.com <tabbatharenea@gmail.com>
2018-06-22 13:16:49 -0400

    updated start page form styles

--- public/js/components/startgame_comp.js
+++ public/js/components/startgame_comp.js
@@ -7,18 +7,26 @@ const startGame ={
     
          <h3>Enter Your Name and Age to defeat Villains of the Galaxy </h3>
         <input type="text" placeholder="Name" ng-model="$ctrl.user.name">
-        <div class="userInfoForm_Grade">
-            <p class="grade animated" ng-click="$ctrl.setGrade(6, $event);">6TH GRADE!</p>
-            <p class="grade animated" ng-click="$ctrl.setGrade(7, $event);">7TH GRADE!</p>
-            <p class="grade animated" ng-click="$ctrl.setGrade(8, $event);">8TH GRADE!</p>
-         </div>
-         <div class="userInfoForm_Subject">
-         <p class="subject animated" ng-click="$ctrl.setSubject(Eng, $event);">ENGLISH</p>
-         <p class="subject animated" ng-click="$ctrl.setSubject(Mat, $event);">MATH</p>
-         <p class="subject animated" ng-click="$ctrl.setSubject(Sci, $event);">SCIENCE</p>
-         <p class="subject animated" ng-click="$ctrl.setSubject(His, $event);">HISTORY</p>
-      </div>
-        <img class="beginAdv" ng-click="$ctrl.sendUserInfo($ctrl.user)" src="../../assets/design/readyToBattle-01.png"</button>
+        
+        <section class="userInfoForm_selections">
+            <div class="userInfoForm_Grade">
+                <h3>Select Your Grade:</h3>
+                <p class="grade animated" ng-click="$ctrl.setGrade(6, $event);">6TH GRADE!</p>
+                <p class="grade animated" ng-click="$ctrl.setGrade(7, $event);">7TH GRADE!</p>
+                <p class="grade animated" ng-click="$ctrl.setGrade(8, $event);">8TH GRADE!</p>
+            </div>
+
+            <div class="userInfoForm_Subject">
+                <h3>Select Your Subject:</h3>
+                <p class="subject animated" ng-click="$ctrl.setSubject('English', $event);">ENGLISH</p>
+                <p class="subject animated" ng-click="$ctrl.setSubject('Math', $event);">MATH</p>
+                <p class="subject animated" ng-click="$ctrl.setSubject('Science', $event);">SCIENCE</p>
+                <p class="subject animated" ng-click="$ctrl.setSubject('History', $event);">HISTORY</p>
+            </div>
+        </section>
+
+        <img class="beginAdv" ng-click="$ctrl.sendUserInfo($ctrl.user)" src="../../assets/design/readyToBattle-01.png">
+    
       </form>
     `,
 
@@ -32,32 +40,26 @@ const startGame ={
 
         vm.setGrade = (grade, $event) =>{
             console.log($event.target);
-            
             console.log(allGrades);
             
             angular.element(allGrades).css("background-color", "red");
-        
-            // angular.element(allGrades).css("box-shadow", "none");
 
             angular.element($event.target).css("background-color", "#000000");
             angular.element($event.target).addClass("rubberBand");
-            // angular.element($event.target).css("box-shadow", "3px 2 px 3px 3px black");
             vm.user.grade = grade;
         }
          
         vm.setSubject = (subject, $event) =>{
                 console.log($event.target);
-                
-                console.log(allSubjects);
+                console.log(subject);
                 
                 angular.element(allSubjects).css("background-color", "red");
             
-                //  angular.element(allGrades).css("box-shadow", "none");
-
-                angular.element($event.target).css("background-color", "#000000");
+                angular.element($event.target).css("background-color", "purple");
                 angular.element($event.target).addClass("rubberBand");
-                // angular.element($event.target).css("box-shadow", "3px 2 px 3px 3px black");
                 vm.user.subject = subject;
+                console.log(vm.user);
+                
         }
 
          vm.sendUserInfo = (user) => {
