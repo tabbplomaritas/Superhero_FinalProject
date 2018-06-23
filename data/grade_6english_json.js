b361d6b2405a69147b11ae19f242f22d6df5c9ea
Donovan <dpaton.dp@gmail.com>
2018-06-23 13:22:58 -0400

    Added grade 6 English Math and Science ??s

--- data/grade_6english_json.js
+++ data/grade_6english_json.js
@@ -3,68 +3,207 @@ const questions=
 [
   // 6th Grade English
   [
-    { "question":"I am happy to report that Grace’s baby is very ______ and ______!",
-      "options": ["happy and healthy","sickly and scrawny","tiny and small","D. hundreds"],
+    {
+      "question":"I am happy to report that Grace’s baby is very ______ and ______!",
+      "options": ["A.happy and healthy","B.sickly and scrawny","tiny and small","D.hungry and annoying"],
       "answer":"happy and healthy"
     },
     {
       "question":"The riverbed was _______ and ______, making it hard to balance well",
-      "options": ["fishy and froggy","shallow and deep","moist and wet","rocky and slippery"],
+      "options": ["A.fishy and froggy","B.shallow and deep","C.moist and wet","D.rocky and slippery"],
       "answer":"rocky and slippery"
     },
     {
         "question":"She was brave _ a lion. What's the missing word",
-        "options": ["as","becuase","before","so"],
+        "options": ["A.as","B.becuase","C.before","D.so"],
         "answer":"as"
     },
      {
       "question":"The car belonging to the manager,Choose the correct answer for the following phrase",
-       "options":["The managers car","The manager's car","The managers'car","The managers's car"],
+       "options":["A.The managers car","B.The manager's car","C.The managers'car","D.The managers's car"],
        "answer":"The manager's car"
 
     },
     {
-      "question":"Their business's success was far greater than what even they themselves had dared to imagine",
-       "options":["Reflexive","Intensive"],
-      "answer":"Intensive"
+      "question":"James took the book; he gave it to the teacher, Choose the pronoun",
+      "options":["A.book","B.he","C.James","D.teacher"],
+     "answer":"Intensive"
+
     },
     {
       "question":"Their business's success was far greater than what even they themselves had dared to imagine",
-      "options":["Reflexive","Intensive"],
+      "options":["A.Reflexive","B.Intensive"],
      "answer":"Intensive"
    },
    {
     "question":"The root therm means heat. What does the word thermostat mean?",
-    "options":["a device that controls the lighting a room",
-              "a device that controls the music a room",
-              "a device that controls the heating a room"],                                        
-    "answer":"a device that controls the heating a room"
+    "options":["A.a device that controls the lighting a room",
+              "B.a device that controls the music a room",
+              "C.a device that controls the heating a room"],                                        
+    "answer":"D.a device that controls the heating a room"
    },
    {
     "question":" What are homonyms?", 
-    "options":["Words that imitate sounds or ideas",
-              "Words that are spelled the same but in their given context have different meanings",
-              "Words that describe, identify or further define a noun or a pronoun"],                                        
-    "answer":"Words that are spelled the same but in their given context have different meanings"
+    "options":["A.Words that imitate sounds or ideas",
+              "B.Words that are spelled the same but in their given context have different meanings",
+              "C.Words that describe, identify or further define a noun or a pronoun"],                                        
+    "answer":"B.Words that are spelled the same but in their given context have different meanings"
    },
    {
     "question":"What is a metaphor?",
-    "options":["A word that replaces one or more nouns",
-              "A word that follows a conjunction",
-              "A word that shows possession",
-            "A word that is implied as being the “same” thing"],                                        
-    "answer":"A word that is implied as being the “same” thing"
+    "options":["A.word that replaces one or more nouns",
+              "B.word that follows a conjunction",
+              "C.word that shows possession",
+            "D.word that is implied as being the “same” thing"],                                        
+    "answer":"D.word that is implied as being the “same” thing"
+    },
+    {
 
-   },
-
-
-    
-
-
+      "question":" That meal should stick to his ribs for a while!",
+      "options":["A.make him feel hungry",
+      "B.last",
+      "C.cause him indigestion",
+      "D.remain in his stomach"],
+      "answer":"B.last"
+    }
   ]
 ]
+       //6th grade Math
+[
+  [
+    {
+     "question":"7 + -3 = ?",
+      "options":["A.-4","B.10","C.4","D.-10"],
+     "answer":"C.4"
+    },
+    {
+      "question":"5÷5 * 3?",
+      "options":["A.3","B.15","C.1","D.4"],
+      "answer":"A.3"
+    },
+    {
+    "question":"3 * 2 + 4?",
+    "options":["A.10","B.11","C.5","D.7"],
+    "answer":"A.10"
+     },
+     {
+      "question":"4 + -3 = ",
+      "options":["A.2","B.1","C.-2","D.0"],
+      "answer":"B.1"
 
+     },
+     {
+      "question":"92,982 – 91,900 = ",
+      "options":["A.1033","B.1082","C.1043","D.1039"],
+      "answer":"B.1082"
 
+     },
+     {
+      "question":"Is -10 × 1 positive or negative?",
+      "options":["A.positive","B.negative"],
+      "answer":"B.negative"
+    },
+    {
+      "question":"Are the ratios 10:14 and 5:7 equivalent?",
+      "options":["A.Yes","B.No"],
+      "answer":"A.Yes"
+    },
+    {
+      "question":"Select the ratio equivalent to  7:6?",
+      "options":["A.63:54","B.21:19","C.43:36","D.77:65"],
+      "answer":"A.63:54"
+    },
+    {
+      "question":"Mark can make 42 birthday cakes in 7 days?,How many can he make in 3 days",
+      "options":["A.63:54","B.21:19","C.43:36","D.77:65"],
+      "answer":"A.63:54"
+    },
+    {
+      "question":"An estimated 3 out of every 25 men are left-handed,What percent of men are left-handed?",
+      "options":["A.18","B.50","C.12","D.6"],
+      "answer":"A.C.12"
+    },
+    ]
+  ]
+
+        //   6th grade Science
+   
+  [
+   [
+    {
+     "question":"I have dry scaly skin breathe with lungs and lay shelled eggs,Who am I?",
+      "options":["A.Primates","B.Reptiles","C.Amphibians","D.Cephalopods"],
+     "answer":"B.Reptiles"
+    },
+    {
+      "question":"What is the function of the blood cell?",
+      "options":["A.Carry messages to all over the body",
+                 "B.To control the size of the openings",
+                 "C.To carry oxygen around the body",
+                 "D.Control the flow of blood"],
+      "answer":"C.To carry oxygen around the body"
+    },
+    {
+    "question":"A iron block has a volume of 61 cubic centimeters and a mass of 122 grams.What is its density?",
+    "options":["A.14","B.4","C.44","D.2"],
+    "answer":"D.2"
+     },
+     {
+      "question":"What abbreviation the Table of elements is K",
+      "options":["A.Krypton","B.Holmium","C.Xenon","D.Potassium"],
+      "answer":"D.Potassium"
+
+     },
+     {
+      "question":"What abbreviation in the Table of elements is Sn",
+      "options":["A.Tennessine","B.Tin","C.Sulfur","D.Gold"],
+      "answer":"B.Tin"
+
+     },
+     {
+      "question":"What element has a density of 1.7?",
+      "options":["A.Fluorine","B.Calcium","C.Carbon","D.Chlorine"],
+      "answer":"A.Fluorine"
+    },
+    {
+      "question":"What element has the atomic number of 15?",
+      "options":["A.Phosphorus","B.Sodium","C.Holmium","D.Argon"],
+      "answer":"A.Phosphorus"
+    },
+    {
+      "question":"What year was Neon discoved as an element?",
+      "options":["A.1877","B.1887","C.1898","D.1889"],
+      "answer":"C.1898"
+    },
+    {
+      "question":"What element makes up around 18% of the human body",
+      "options":["A.Oxygen","B.Carbon","C.Hydrogen","D.Sodium"],
+      "answer":"B.Carbon"
+    },
+    {
+      "question":"What are the two types of the earth's crust?",
+      "options":["A.Continental crust and Canopy crust",
+      "B.Underbody crust and Oceanic crust",
+      "C.Continental crust and Oceanic crust","D.Mantle crust and Underbody crust"],
+      "answer":"C.Continental crust and Oceanic crust"
+    },
+    ]
+  ]
+          // Grade 6 history
+  [
+    [
+     {
+      "question":"Who was the fourth President of the United States?",
+       "options":["A.John Adams","B.Thomas Jefferson","C.Alexander Hamilton","D.James Madison"],
+      "answer":"D.James Madison"
+     }
+   
+     ]
+   ]
+ 
+ 
+
+  
 [
   // 7th Grade English
   [
