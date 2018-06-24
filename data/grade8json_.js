3527c34b69640cf499dfffe7df0efe270471c074
unknown <kevin.williams49531@gmail.com>
2018-06-24 09:40:27 -0400

    8th grade English/Math questions

---
+++ data/grade8json_.js
@@ -0,0 +1,107 @@
+// 8th grade english
+[
+  {
+    "question":"The antonym of dirty is ",
+    "options":["A. Beautiful","B. Bad","C. Clean","D. Ugly"],
+    "answer":"C. Clean"
+  },
+  {
+    "question":"What does the word minutiae mean?",
+    "options":["A. artistic or aesthetic details","B. controversial or disputable details","C. unimportant or trifling details","D. negotiable details"],
+    "answer":"C. unimportant or trifling details"
+  },
+  {
+    "question":"What does the word chronological mean?",
+    "options":["A. arranged in the order events occured","B. arranged in a way that can be double-checked","C. arranged in the format required by law","D. arranged in no discernible pattern"],
+    "answer":"A. arranged in the order events occured"
+  },
+  {
+    "question":"What type of sentence is this? 'Since it takes approximately one thousand years for a used printer cartridge to decompose, recycling these plastic ink dispensers helps the environment.'",
+    "options":["A. simple","B. compound","C. complex","D. compound complex"],
+    "answer":"C. complex"
+  },
+  {
+    "question":"What type of sentence is this? 'An avid reader, Ben attends weekly book club meetings, and he finishes several novels every month.'",
+    "options":["A. simple","B. compound","C. complex","D. compound complex"],
+    "answer":"B. compound"
+  },
+  {
+    "question":"Choose either A,B,C or D for the correct answer. She  ... at me.",
+    "options":["A. smile","B. smiles","C. Is smiles","D. is smile"],
+    "answer":"B. smiles"
+  },
+  {
+    "question":"Choose either A,B,C or D for the correct answer. The person .. you met this morning was my uncle",
+    "options":["A. who","B. whom","C. that","D. which"],
+    "answer":"A. who"
+  },
+  {
+    "question":"Which word is a synonym of deficient?",
+    "options":["A. lacking","B. dependent","C. judicious","D. affluent"],
+    "answer":"A. lacking"
+  },
+  {
+    "question":"The choir from Danville performed brilliantly during the final round of the regional competition. 'Perform is a(n)'",
+    "options":["A. adjective","B. adverb","C. clause","D. phrase"],
+    "answer":"B. adverb"
+  },
+  {
+    "question":"Put the words in alphabetical order. 'stake, sack, fowl, formal'",
+    "options":["A. stake, sack, fowl, formal","B. fowl, formal, stake, sack","C. formal, fowl, sack, stack","D. formal, stake, sack, fowl"],
+    "answer":"C. formal, fowl, sack, stack"
+  },
+],
+// 8th grade math
+[
+  {
+    "question":"What numbers are factors of 10",
+    "options":["A. 1 and 4","B. 1 and 2","C. 3 and 9","D. 2 and 6"],
+    "answer":"B. 1 and 2"
+  },
+  {
+    "question":"After earning 700 points on a game show, Malia got to compete in the bonus round. During the bonus round, she lost 100 points on the gardening questions. What was her final score?",
+    "options":["A. 800","B. -600","C. 600","D. -800"],
+    "answer":"C. 600"
+  },
+  {
+    "question":"What is the percent of change from 24 to 36?",
+    "options":["A. 50% decrease","B. 50% increase","C. 38% decrease","D. 38% increase"],
+    "answer":"B. 50% increase"
+  },
+  {
+    "question":"A triangle has angle measurements of 90°, 79°, and 11°. What kind of triangle is it?",
+    "options":["A. acute","B. right","C. left","D. obtuse"],
+    "answer":"B. right"
+  },
+  {
+    "question":"What is 2/4 in lowest terms?",
+    "options":["A. 1/2","B. 1/3","C. 4/5","D. 3/6"],
+    "answer":"A. 1/2"
+  },
+  {
+    "question":"An isosceles triangle has how many equal sides?",
+    "options":["A. 1","B. 2","C. 3","D. 4"],
+    "answer":"B. 2"
+  },
+  {
+    "question":"A triangle has angle measurements of 1°, 177°, and 2°. What kind of triangle is it?",
+    "options":["A. acute","B. right","C. left","D. obtuse"],
+    "answer":"D. obtuse"
+  },
+  {
+    "question":"The radius of a circle is 3 feet. What is the circle's diameter?",
+    "options":["A. 3 feet","B. 1.5 feet","C. 6 feet", "D. 12 feet"],
+    "answer":"C. 6 feet"
+  },
+  {
+    "question":"What is the percent of change from 200,000 to 20,000?",
+    "options":["A. 90% decrease","B. 112% increase","C. 112% decrease","D. 90% increase"],
+    "answer":"A. 90% decrease"
+  },
+  {
+    "question":"According to his bank statement, Seth spent $2,000 on a piano. If Seth's bank account started out with a balance of $8,000, which integer represents the final balance?",
+    "options":["A. 10000","B. 7000","C. 6000","D. 5000"],
+    "answer":"C. 6000"
+  }
+]
+
